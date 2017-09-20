3e8f1f03b3d72f0a98af019198c6bdbf77f205f4
Staelle MAKAMTE <staellesonia@yahoo.fr>
2017-09-20 16:38:26 +0200

    commit1

---
+++ src/com/objis/monapplication/HelloObjis.java
@@ -0,0 +1,10 @@
+package com.objis.monapplication;
+
+public class HelloObjis {
+
+public static void main(String[] args) {
+
+        System.out.println("Formez-vous avec Objis, specialiste formation Java !");
+
+        }
+}
